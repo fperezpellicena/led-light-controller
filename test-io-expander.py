2cc7ad3c625de816af17e99fcc6178ac5b2b04cc
Francisco Perez Pellicena <fperezpellicena@gmail.com>
2022-01-25 08:41:33 +0100

    Updated IOExpander test to support banks of effects

--- test-io-expander.py
+++ test-io-expander.py
@@ -4,36 +4,95 @@ from breakout_ioexpander import BreakoutIOExpander
 
 PINS_PICO_EXPLORER = {"sda": 20, "scl": 21}
 
-ioe_button_pins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-last_states = [True, True, True, True, True, True, True, True, True, True]
-current_states = [True, True, True, True, True, True, True, True, True, True]
-
 i2c = PimoroniI2C(**PINS_PICO_EXPLORER)
 ioe = BreakoutIOExpander(i2c, address=0x18)
 
-def initialiseInputPins():
-    for ioe_button_pin in ioe_button_pins:
-        ioe.set_mode(ioe_button_pin, BreakoutIOExpander.PIN_IN_PU)
+#Banks
+banks = [
+    {"id": 0,"name": "Bank A","pin": 1},
+    {"id": 1,"name": "Bank B","pin": 2},
+    {"id": 2,"name": "Bank C","pin": 3}
+]
+
+# The first bank is active by default
+activeBank = 0
+bankChanged = False
+
+#Effects
+effects = [
+    {"id": 0,"name": "Effect 1","pin": 4},
+    {"id": 1,"name": "Effect 2","pin": 5},
+    {"id": 2,"name": "Effect 3","pin": 6}
+]
+
+# The first effect is active by default
+activeEffect = 0
+effectChanged = False
+
+def initialiseBanks():
+    for bank in banks:
+        ioe.set_mode(bank["pin"], BreakoutIOExpander.PIN_IN_PU)
         
-def readInputPins():
-    for ioe_button_pin in ioe_button_pins:
-        if ioe.input(ioe_button_pin) == 1:
-            current_states[ioe_button_pin - 1] = True
-        else:
-            current_states[ioe_button_pin - 1] = False
+def initialiseEffects():
+    for effect in effects:
+        ioe.set_mode(effect["pin"], BreakoutIOExpander.PIN_IN_PU)
+
+def updateActiveBank():
+    global activeBank
+    global bankChanged
+    for bank in banks:
+        if(not ioe.input(bank["pin"])):
+            if (activeBank is not bank["id"]):
+                bankChanged = True
+                activeBank = bank["id"]
+                activateFirsEffectOfBank()
+                print(banks[activeBank]["name"] + " and " + effects[activeEffect]["name"] + " actives")
+            break
         
-def checkInputPins():
-    for ioe_button_pin in ioe_button_pins:
-        current_state = current_states[ioe_button_pin - 1]
-        last_state = last_states[ioe_button_pin - 1]
-        if current_state is not last_state:
-            print("Button " + str(ioe_button_pin) + " has been switched from " + str(last_state) + " to " + str(current_state))
+def activateFirsEffectOfBank():
+    global activeEffect
+    activeEffect = 0
         
-        last_states[ioe_button_pin - 1] = current_state
+def updateActiveEffect():
+    global activeEffect
+    global effectChanged
+    for effect in effects:
+        if(not ioe.input(effect["pin"])):
+            if (activeEffect is not effect["id"]):
+                effectChanged = True
+                activeEffect = effect["id"]
+                print(banks[activeBank]["name"] + " and " + effects[activeEffect]["name"] + " actives")
+            break
+        
+def effectOrBankChanged():
+    global bankChanged
+    global effectChanged
+    updateActiveBank()
+    updateActiveEffect()
+    if (bankChanged or effectChanged):
+        print("Bank or effect changed!")
+        bankChanged = False
+        effectChanged = False
+        return True
+    else:
+        return False
+
+def runActiveBankEffect():
+    while(not effectOrBankChanged()):
+        print("Start effect (" + str(activeBank) + ":" + str(activeEffect) + ") [" + banks[activeBank]["name"] + " and " + effects[activeEffect]["name"] + "]")
+        time.sleep(1)
+        if (effectOrBankChanged()):
+            break
+        time.sleep(1)
+        if (effectOrBankChanged()):
+            break
+        print("End effect [" + banks[activeBank]["name"] + " and " + effects[activeEffect]["name"] + "]")
     
-initialiseInputPins()
+initialiseBanks()
+initialiseEffects()
 
 while True:
-    readInputPins();
-    checkInputPins();
-    time.sleep(0.25)
+    updateActiveBank()
+    updateActiveEffect()
+    runActiveBankEffect()
+    # time.sleep(0.25)
